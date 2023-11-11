42c087bc792a66f878ac8cdbfe8e975cfde65406
Nguyen-Van-Truong <20130187@st.hcmuaf.edu.vn>
2023-11-11 15:26:45 +0700

    Giao dien Home, Dang nhap, Dang ky

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "SocialApp",
-      home: Home(),
+      home: Login(),
     );
   }
 
--- lib/views/screen/auth/login.dart
+++ lib/views/screen/auth/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:social_app/views/screen/auth/register.dart';
+import 'package:social_app/views/screen/home.dart';
 
 class Login extends StatelessWidget {
   @override
@@ -40,7 +41,7 @@ class Login extends StatelessWidget {
           _buildTextField(hintText: "Email or Phone number", obscureText: false),
           _buildTextField(hintText: "Password", obscureText: true),
           SizedBox(height: 30),
-          _buildLoginButton(),
+          _buildLoginButton(context),
           SizedBox(height: 70),
           GestureDetector(
             onTap: () {
@@ -91,22 +92,28 @@ class Login extends StatelessWidget {
     );
   }
 
-  Widget _buildLoginButton() {
-    return Container(
-      height: 50,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        gradient: LinearGradient(
-          colors: [
-            Color.fromRGBO(143, 148, 251, 1),
-            Color.fromRGBO(143, 148, 251, .6),
-          ],
+  Widget _buildLoginButton(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Thực hiện điều hướng đến màn hình Home
+        Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+      },
+      child: Container(
+        height: 50,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(143, 148, 251, 1),
+              Color.fromRGBO(143, 148, 251, .6),
+            ],
+          ),
         ),
-      ),
-      child: Center(
-        child: Text(
-          "Login",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        child: Center(
+          child: Text(
+            "Login",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
     );
