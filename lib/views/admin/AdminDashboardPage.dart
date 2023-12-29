28701fc98d370767cf968ac7a7fd6af504e64633
Nguyen-Van-Truong <20130187@st.hcmuaf.edu.vn>
2023-12-29 15:43:50 +0700

    thống kê: tổng số người dùng, tổng số người dùng thường, tổng số người dùng admin, tổng số bài viết, người dùng tích cực nhất (nhiều bài viết nhất), người dùng có nhiều theo dõi nhất, người dùng có nhiều bạn mới sửa lại cách chọn màu

--- lib/views/admin/AdminDashboardPage.dart
+++ lib/views/admin/AdminDashboardPage.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:social_app/config.dart';
-
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import '../screen/auth/Login.dart';
 
 class AdminDashboardPage extends StatefulWidget {
@@ -11,41 +13,62 @@ class AdminDashboardPage extends StatefulWidget {
 }
 
 class _AdminDashboardPageState extends State<AdminDashboardPage> {
-  final List<Color> _colors = [
-    Colors.red,
-    Colors.green,
-    Colors.blue,
-    Colors.orange,
-    Colors.purple,
-    Colors.yellow,
-    Colors.teal,
-    Color(0xFF00FF00),
-    Color(0xFF123456),
-    Colors.pink,
-    Colors.cyan,
-    Color(0xFF654321),
-    Colors.indigo,
-    Colors.amber,
-    Color(0xFFABCDEF),
-    Colors.deepPurple,
-    Color(0xFFAABBCC),
-    Colors.lime,
-    Colors.brown,
-    Color(0xFF654321),
-    Colors.grey,
-    Color(0xFF999999),
-    Colors.lightGreen,
-    Colors.deepOrange,
-    // Add more colors as needed
-  ];
+  Map<String, dynamic> _statistics = {};
 
+  @override
+  void initState() {
+    super.initState();
+    _fetchStatistics();
+  }
 
+  Future<void> _fetchStatistics() async {
+    final response = await http.get(Uri.parse('${Config.BASE_URL}/api/statistics/getStatistics.php'));
 
-  Future<void> _updateThemeColor(Color color) async {
+    if (response.statusCode == 200) {
+      setState(() {
+        _statistics = jsonDecode(response.body)['statistics'];
+      });
+    } else {
+      print("Failed to fetch statistics");
+    }
+  }
+
+  Future<void> _updateThemeColor() async {
+    Color pickedColor = Colors.blue; // Default color
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Pick a color!'),
+          content: SingleChildScrollView(
+            child: ColorPicker(
+              pickerColor: pickedColor,
+              onColorChanged: (Color color) {
+                pickedColor = color;
+              },
+              showLabel: true,
+              pickerAreaHeightPercent: 0.8,
+            ),
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              child: const Text('Done'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _sendColorToApi(pickedColor);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _sendColorToApi(Color color) async {
     final response = await http.post(
       Uri.parse('${Config.BASE_URL}/api/appsettings/themeColor.php'),
       body: {
-        'color': color.value.toString(), // Send color value to your API
+        'color': color.value.toString(),
       },
     );
 
@@ -59,7 +82,6 @@ class _AdminDashboardPageState extends State<AdminDashboardPage> {
   Future<void> _logout() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.clear(); // Clear all shared preferences
-
     Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => Login()));
   }
 
@@ -82,25 +104,10 @@ class _AdminDashboardPageState extends State<AdminDashboardPage> {
             padding: const EdgeInsets.all(8.0),
             child: Text('Theme Color Settings', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
           ),
-          Container(
-            height: 100,
-            child: GridView.builder(
-              scrollDirection: Axis.horizontal,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 1,
-                crossAxisSpacing: 8.0,
-                mainAxisSpacing: 8.0,
-              ),
-              itemCount: _colors.length,
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () => _updateThemeColor(_colors[index]),
-                  child: Container(
-                    color: _colors[index],
-                  ),
-                );
-              },
-            ),
+          ListTile(
+            leading: Icon(Icons.color_lens),
+            title: Text('Select Theme Color'),
+            onTap: _updateThemeColor,
           ),
           Divider(),
           Padding(
@@ -108,16 +115,40 @@ class _AdminDashboardPageState extends State<AdminDashboardPage> {
             child: Text('Statistics', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
           ),
           ListTile(
-            leading: Icon(Icons.analytics),
-            title: Text('Number of Users'),
-            subtitle: Text('1000'), // Replace with dynamic data
+            leading: Icon(Icons.person),
+            title: Text('Total Number of Users'),
+            subtitle: Text('${_statistics['total_users'] ?? 'Loading...'}'),
+          ),
+          ListTile(
+            leading: Icon(Icons.person_outline),
+            title: Text('Total Users with Role: User'),
+            subtitle: Text('${_statistics['total_users_with_role_user'] ?? 'Loading...'}'),
+          ),
+          ListTile(
+            leading: Icon(Icons.admin_panel_settings),
+            title: Text('Total Users with Role: Admin'),
+            subtitle: Text('${_statistics['total_users_with_role_admin'] ?? 'Loading...'}'),
           ),
           ListTile(
             leading: Icon(Icons.post_add),
             title: Text('Number of Posts'),
-            subtitle: Text('500'), // Replace with dynamic data
+            subtitle: Text('${_statistics['total_posts'] ?? 'Loading...'}'),
           ),
-          // Add more statistics as needed
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text('Most Active User'),
+            subtitle: Text('${_statistics['most_active_user']?['username'] ?? 'Loading...'} - Posts: ${_statistics['most_active_user']?['post_count'] ?? ''}'),
+          ),
+          ListTile(
+            leading: Icon(Icons.group),
+            title: Text('User with Most Followers'),
+            subtitle: Text('${_statistics['most_followed_user']?['username'] ?? 'Loading...'} - Followers: ${_statistics['most_followed_user']?['follower_count'] ?? ''}'),
+          ),
+          ListTile(
+            leading: Icon(Icons.people),
+            title: Text('User with Most Friends'),
+            subtitle: Text('${_statistics['user_with_most_friends']?['username'] ?? 'Loading...'} - Friends: ${_statistics['user_with_most_friends']?['friend_count'] ?? ''}'),
+          ),
         ],
       ),
     );
